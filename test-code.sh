f131364d777dcc361f706bd204161accae5d671a
rivalent <richardvalente20052005@gmail.com>
2026-04-26 12:28:27 -0300

    corrigindo o arquivo de testes

--- test-code.sh
+++ test-code.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "Instalando dependências do python:"
+echo "Instalando dependências e criando ambiente virtual do python:"
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -q -r requirements.txt
 
 echo "Iniciando testes unitários"
 PYTHONPATH=. pytest tests/unit/ -v
 echo "Testes unitários finalizados"
+deactivate
